66df914b51cd50da60745ba327c5e793a3c4d5ef
Your Name <jigneshmakvana077@gmail.com>
2025-02-21 10:31:47 +0530

    add to all URL

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -16,8 +16,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
   origin: [
-    'https://luxora-puce.vercel.app',
-    'http://localhost:5173'
+    '*'
   ],
   
   credentials: true
