c0f22d2b4718e520a806c0cef4b654d9fd008c6f
Your Name <jigneshmakvana077@gmail.com>
2025-02-22 08:34:25 +0530

    Production error

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-  origin: ['https://luxora-puce.vercel.app'], // Allow only your frontend
+  origin: 'https://luxora-puce.vercel.app',
   credentials: true
 }));
 
